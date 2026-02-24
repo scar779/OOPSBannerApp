bc6a0f6ba65fa88fd20dd4fd165ee06be9ab1f3e
scar779 <76441862+scar779@users.noreply.github.com>
2026-02-24 15:49:11 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the Java application
+     * 
+     * @param args Command line arguments (not used in this application)
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
