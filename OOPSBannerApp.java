025e1a4415d50bd885c30e2ad01f86baf111b513
scar779 <76441862+scar779@users.noreply.github.com>
2026-03-02 00:48:12 +0530

    Coded to Render OOPS as Banner using Print Statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,13 @@
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the Java application
-     * 
-     * @param args Command line arguments (not used in this application)
-     */
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
+        System.out.println("**     **" + "**     **" + "**     **" + "**      ");
+        System.out.println("**     **" + "**     **" + " ****** " + "  *****  ");
+        System.out.println("**     **" + "**     **" + " **      " + "      ** ");
+        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
     }
 }
